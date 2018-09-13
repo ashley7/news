e1c2f1bfd3c35fa73168aaf6db402a62bf5fdc8f
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-09-13 11:59:56 +0300

    Making clean ups routes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        '/news.index',
-        '/news.show',
-        '/news.store',
-        '/home/',
+        'news.index',
+        'news.show',
+        'news.store',
+        'news.edit',
+        'home',
         '/number_of_articals',
     ];
 }
