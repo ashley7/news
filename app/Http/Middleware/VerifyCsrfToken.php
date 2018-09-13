0ec7012668516ee8c43274ce0880e4a444aa6d7a
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-09-13 13:41:22 +0300

    cleans Kills Authentication

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        'news.index',
-        'news.show',
-        'news.store',
-        'news.edit',
-        'home',
-        '/number_of_articals',
+      
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('news','NewContentController');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/number_of_articals','NewContentController@number_of_articals');
