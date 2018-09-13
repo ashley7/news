8c970cb95ecc633b5bb6a50bd5948daa413c9860
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-09-13 11:47:02 +0300

    Making clean ups

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,9 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
     protected $except = [
-        //
+        '/news/*',
+        '/home/',
+        '/number_of_articals',
     ];
 }
